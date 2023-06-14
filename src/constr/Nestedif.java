13cbba6ee81809a867517852433d804310640578
YV SAI MOUNIKA <YV SAI MOUNIKA@LAPTOP-0SVG1LN0>
2023-06-14 16:22:21 +0530

    uploading

---
+++ src/constr/Nestedif.java
@@ -0,0 +1,36 @@
+package constr;
+
+public class Nestedif {
+
+	public static void main(String[] args) {
+		
+		int a=30;
+		int b=40;
+		
+	
+		if (a==30) {
+			
+			if(b==50) {
+				
+				System.out.println("a value is 30& b value is 40");
+			}
+			
+			
+ 		
+		else if (a==40)
+		
+		{
+			
+			System.out.println(" else statement of b value is 30");
+		}
+
+	}
+		
+		else 
+		
+		{
+			System.out.println("else stmt of 1st condition");
+		}
+		
+		}
+}
